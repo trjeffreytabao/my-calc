44cbe989965483fb4dd2c97f6e4175c288eec176
trjeffreytabao <jeffrey.tabao@tooltwist.com>
2023-01-30 14:15:52 +0800

    edited_layout_final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Button({ number }) {
+  return <button>{number}</button>;
+}
 
 function App() {
   return (
+    <div class="container">
+      <div class="container-calc">
+        <div class="container-screen-calc">
+          <div class="output_screen"></div>
+        </div>
+        <div class="container-buttons-calc">
+          <Button number={7} />
+          <Button number={8} />
+          <Button number={9} />
+          <Button number={"/"} />
+          <Button number={4} />
+          <Button number={5} />
+          <Button number={6} />
+          <Button number={"x"} />
+          <Button number={1} />
+          <Button number={2} />
+          <Button number={3} />
+          <Button number={"-"} />
+          <Button number={"CLR"} />
+          <Button number={0} />
+          <Button number={"="} />
+          <Button number={"+"} />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default App;
+
+/*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,7 +52,4 @@ function App() {
         </a>
       </header>
     </div>
-  );
-}
-
-export default App;
+    */
